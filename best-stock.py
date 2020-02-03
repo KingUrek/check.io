067ee66679580c9c7cbeff78a57a6d02f585b8c7
Gabriel Lucas <dreadpool38@gmail.com>
2020-02-03 20:01:01 -0300

    Best solution to best-stock

--- best-stock.py
+++ best-stock.py
@@ -17,3 +17,12 @@ if __name__ == '__main__':
     assert best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}) == "ATX"
     assert best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}) == "TASI"
     print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+
+
+"""best solution
+
+def best_stock(data):
+    return max(data, key=data.__getitem__)
+"""
